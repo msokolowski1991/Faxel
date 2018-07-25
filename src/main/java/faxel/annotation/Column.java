27eae125a30c4ebac803f4c85939b46af5f4238e
Michael <m.sokolowski.1991@gmail.com>
2018-07-25 21:43:38 +0200

    @Sheet and @Column annotations

--- src/main/java/faxel/annotation/Column.java
+++ src/main/java/faxel/annotation/Column.java
@@ -1,5 +1,13 @@
 package faxel.annotation;
 
+import java.lang.annotation.*;
+
+/**
+ * Annotation used to mark a field of a class as an column representation
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
 public @interface Column {
     int index();
 }
--- src/main/java/faxel/annotation/Sheet.java
+++ src/main/java/faxel/annotation/Sheet.java
@@ -1,6 +1,15 @@
 package faxel.annotation;
 
+import java.lang.annotation.*;
+
+/**
+ * Annotation used to mark a Collection as Sheet of Workbook.
+ * Collection will be then populated with sheet rows.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
 public @interface Sheet {
     String name();
-    int firstDataRow();
+    int firstDataRow() default 1;
 }
