7ebc78f6aecb30f55a194f116e63ad0b1d87fc70
Michael <m.sokolowski.1991@gmail.com>
2018-07-26 23:49:42 +0200

    Documentation

--- src/main/java/faxel/annotation/SheetRows.java
+++ src/main/java/faxel/annotation/SheetRows.java
@@ -10,6 +10,14 @@ import java.lang.annotation.*;
 @Target(ElementType.FIELD)
 @Documented
 public @interface SheetRows {
+
+    /**
+     * Name of sheet - source of data of this Collection
+     */
     String sheetName();
+
+    /**
+     * Number of row to start from parsing. Default is first row.
+     */
     int firstDataRow() default 1;
 }
