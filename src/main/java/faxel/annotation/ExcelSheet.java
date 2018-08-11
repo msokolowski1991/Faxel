60ca6b935ac10085bfe805593820859aea2c3138
Michael <m.sokolowski.1991@gmail.com>
2018-08-11 22:25:32 +0200

    README

--- src/main/java/faxel/annotation/ExcelSheet.java
+++ src/main/java/faxel/annotation/ExcelSheet.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 /**
  * Annotation used to mark a Collection as ExcelSheet of Workbook.
  * Collection will be then populated with sheet rows.
- * One of name() or index() must be provided
+ * One of  {@link #name() name} or {@link #index() index}  must be provided.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
@@ -14,12 +14,14 @@ public @interface ExcelSheet {
 
     /**
      * Name of sheet - source of data of this Collection
+     *
      * @return Name of the sheet
      */
     String name() default "";
 
     /**
      * Index of sheet - source of data of this Collection
+     *
      * @return Index of the sheet
      */
     int index() default -1;
@@ -27,22 +29,24 @@ public @interface ExcelSheet {
     /**
      * Indicates how to interpret data arrangement in this sheet.
      * Possible values are ROW or COLUMN
+     *
      * @return Desired data arrangement type. Default is ROW
      */
     DataArrangementType arrangement() default DataArrangementType.ROW;
 
     /**
-     * Number of row or column to startPosition from parsing. Default is first row.
-     * @return index of first row or column to startPosition processing from.
+     * Index of row or column to startPosition from parsing. Default is first row.
+     *
+     * @return index of first row or column to start processing from.
      */
     int startPosition() default 1;
 
     /**
-     * Number of maxPosition positioned row or column to parse. Default is Integer.MAX_VALUE.
-     * If the actual length of data set is greater than this value - it will be ignored.
+     * Index of maxPosition positioned row or column to parse. Default is Integer.MAX_VALUE.
+     * If the actual length of data set is greater than this value then maxPosition will be ignored.
+     *
      * @return index of last row or column to process.
      */
     int maxPosition() default Integer.MAX_VALUE;
-
 
 }
