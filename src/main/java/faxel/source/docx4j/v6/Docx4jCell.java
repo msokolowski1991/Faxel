1d20df2289667adb320624ee8445f90a74d5a25d
Michael <m.sokolowski.1991@gmail.com>
2019-03-11 22:59:54 +0100

    Feature/docx4j_impl: improved error messages in Docx4jCell. Proper cell type name is recognized.

--- src/main/java/faxel/source/docx4j/v6/Docx4jCell.java
+++ src/main/java/faxel/source/docx4j/v6/Docx4jCell.java
@@ -2,6 +2,7 @@ package faxel.source.docx4j.v6;
 
 import faxel.FaxelException;
 import faxel.source.SourceCell;
+import org.xlsx4j.model.CellUtils;
 import org.xlsx4j.org.apache.poi.ss.usermodel.DataFormatter;
 import org.xlsx4j.org.apache.poi.ss.usermodel.DateUtil;
 import org.xlsx4j.sml.Cell;
@@ -28,7 +29,7 @@ class Docx4jCell implements SourceCell {
             return null;
         }
         if (cellType != STCellType.S) {
-            throw new FaxelException("Illegal cell type %s. Could not get string value", cellType.value());
+            throw new FaxelException("Illegal cell type %s. Could not get string value", getCellTypeName());
         }
         return dataFormatter.formatCellValue(cell);
     }
@@ -36,7 +37,7 @@ class Docx4jCell implements SourceCell {
     @Override
     public double numericValue() {
         if (cellType != STCellType.N) {
-            throw new FaxelException("Illegal cell type %s. Could not get numeric value", cellType.value());
+            throw new FaxelException("Illegal cell type %s. Could not get numeric value", getCellTypeName());
         }
         if (stringValue == null) {
             return 0;
@@ -47,7 +48,7 @@ class Docx4jCell implements SourceCell {
     @Override
     public boolean boolValue() {
         if (cellType != STCellType.B && cellType != STCellType.N) {
-            throw new FaxelException("Illegal cell type %s. Could not get boolean value", cellType.value());
+            throw new FaxelException("Illegal cell type %s. Could not get boolean value", getCellTypeName());
         }
         return "1".equals(stringValue) || Boolean.valueOf(stringValue);
     }
@@ -55,11 +56,31 @@ class Docx4jCell implements SourceCell {
     @Override
     public Date dateValue() {
         if (cellType != STCellType.N) {
-            throw new FaxelException("Illegal cell type %s. Could not get date value", cellType.value());
+            throw new FaxelException("Illegal cell type %s. Could not get date value", getCellTypeName());
         }
         if (stringValue == null) {
             return null;
         }
         return DateUtil.getJavaDate(Double.valueOf(stringValue));
+    }
+
+    private String getCellTypeName() {
+        int cellType = CellUtils.getCellType(cell);
+        switch (cellType) {
+            case CellUtils.CELL_TYPE_NUMERIC:
+                return "numeric";
+            case CellUtils.CELL_TYPE_STRING:
+                return "string";
+            case CellUtils.CELL_TYPE_BOOLEAN:
+                return "boolean";
+            case CellUtils.CELL_TYPE_BLANK:
+                return "blank";
+            case CellUtils.CELL_TYPE_ERROR:
+                return "error";
+            case CellUtils.CELL_TYPE_FORMULA:
+                return "formula";
+            default:
+                return "unknown";
+        }
     }
 }
